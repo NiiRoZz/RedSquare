c17b8285c2982b860276aa58b89d447df02f1d59
Flo <florian.vetter@hotmail.fr>
2019-11-06 21:10:08 +0100

    update chat.cc with box

--- code/client/Chat.cc
+++ code/client/Chat.cc
@@ -15,32 +15,44 @@ namespace redsquare
 
     void Chat::updateChat(gf::UI &chatUI)
     {
-        
+        static gf::UICharBuffer box(512);
+         static gf::UICharBuffer text(64);
+
         static gf::Color4f comboColor1 = gf::Color::fromRgba32(255, 255, 255, 100);
-       if( chatUI.begin("Chat", gf::RectF(0, 360, 220, 220), gf::UIWindow::Border | gf::UIWindow::Title|gf::UIWindow::NoScrollbar)){
-           
-           static gf::UICollapse groupCollapsed = gf::UICollapse::Minimized;
-           
-                chatUI.layoutRowStatic(180, 210, 1);
-                if (chatUI.groupBegin("", gf::UIWindow::Border))
-                {
-                    chatUI.layoutRowStatic(18, 100, 1);
-                    chatUI.label("test");
-                    chatUI.label("test\n ggg");
-                    chatUI.label("test");
-                    chatUI.label("test");
-                    chatUI.label("test");
-                    chatUI.label("test");
-                    chatUI.label("test");
-                    chatUI.label("test");
-
-                    chatUI.label("test");
-                    chatUI.label("test");
-
-                    chatUI.groupEnd();
-                }
+        chatUI.setCharacterSize(10);
+        if( chatUI.begin("Chat", gf::RectF(0, 360, 220, 220), gf::UIWindow::Border | gf::UIWindow::Title|gf::UIWindow::NoScrollbar)){
             
+            static gf::UICollapse groupCollapsed = gf::UICollapse::Minimized;
+            
+            chatUI.layoutRowStatic(130, 200, 1);
+            if (chatUI.groupBegin("", gf::UIWindow::Border))
+            {
+                chatUI.layoutRowStatic(50, 175, 1);
+                chatUI.edit(gf::UIEditType::Box | gf::UIEdit::ReadOnly, box);
+
+                chatUI.layoutRowStatic(50, 175, 1);
+                chatUI.edit(gf::UIEditType::Box | gf::UIEdit::ReadOnly, box);
+
+                chatUI.groupEnd();
+            }
+            chatUI.layoutRowStatic(50, 200, 1);
+
+            if (chatUI.groupBegin("test", gf::UIWindow::Border))
+            {
+                chatUI.layoutRowBegin(gf::UILayout::Static, 20, 2);
+                chatUI.layoutRowPush(120);
+                gf::UIEditEventFlags flags = chatUI.edit(gf::UIEditType::Field | gf::UIEdit::SigEnter, text, gf::UIEditFilter::Ascii);
+                chatUI.layoutRowPush(50);
+                
+                if (chatUI.buttonLabel("Submit") || flags.test(gf::UIEditEvent::Commited))
+                {
+                    box.append(text);
+                    text.clear();
+                }
+                chatUI.layoutRowEnd();
+                chatUI.groupEnd();
+            } 
             chatUI.end();
-       }
+        }
     }
 }
