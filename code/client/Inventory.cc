e1d61397cfc959f22098555338349ae7d26433b5
Flo <florian.vetter@hotmail.fr>
2019-11-21 13:10:23 +0100

    Add stuff case and player case

--- code/client/Inventory.cc
+++ code/client/Inventory.cc
@@ -21,26 +21,41 @@ namespace redsquare
 
     void Inventory::update(gf::Time time)
     {
-        static gf::UICharBuffer text(64);
         if(m_ShowInventory){
             m_UI.setCharacterSize(18);
 
-            if( m_UI.begin("Inventory", gf::RectF::fromPositionSize( {400, 90}, {535, 400} ),gf::UIWindow::Title|gf::UIWindow::NoScrollbar))
+            if( m_UI.begin("Inventory", gf::RectF::fromPositionSize( {400, 90}, {535, 405} ),gf::UIWindow::Title|gf::UIWindow::NoScrollbar))
             {
-                m_UI.layoutRowStatic(80, 80, 1);
+                m_UI.layoutRowStatic(140, 140, 3);
 
-                if (m_UI.groupBegin(""))
+                if (m_UI.groupBegin("", gf::UIWindow::NoScrollbar))
                 {
-                    m_UI.layoutRowDynamic(32, 1);
+                    m_UI.layoutRowStatic(140,128, 1);
                     m_UI.image(gResourceManager().getTexture("img/attackCursor.png"),gf::RectF());
                     m_UI.groupEnd();
                 }
+                if (m_UI.groupBegin("Stuff", gf::UIWindow::NoScrollbar))
+                {
+                    m_UI.layoutRowStatic(32,32, 1);
+                    m_UI.image(gResourceManager().getTexture("img/attackCursor.png"),gf::RectF());
+
+                    m_UI.layoutRowStatic(32,32, 1);
+                    m_UI.image(gResourceManager().getTexture("img/attackCursor.png"),gf::RectF());
+
+                    m_UI.layoutRowStatic(32,32, 1);
+                    m_UI.image(gResourceManager().getTexture("img/attackCursor.png"),gf::RectF());
+
+                    m_UI.layoutRowStatic(32,32, 1);
+                    m_UI.image(gResourceManager().getTexture("img/attackCursor.png"),gf::RectF());
+                    m_UI.groupEnd();
+                }
+                
 
                 m_UI.layoutRowStatic(350, 525, 1);
                 int i=0;
                     if (m_UI.groupBegin(""))
                     {
-                        for (int i =0; i<7;i++){
+                        for (int i =0; i<6;i++){
                         m_UI.layoutRowDynamic(32, 14);
                         
                             for(int y =0; y<14;y++){
