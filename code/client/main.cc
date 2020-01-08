a1194446a464b6c772891b86d7a56c10df014f14
VitizLaPeste <lucas.grosjean07@orange.fr>
2020-01-08 21:57:44 +0100

    Updated usage message for Rogue

--- code/client/main.cc
+++ code/client/main.cc
@@ -43,7 +43,7 @@ int main( int argc, char **argv )
     if ( argc != 5 )
     {
         std::cerr << "Usage : ./RedSquare ip port playerName typePlayer" << std::endl;
-        std::cerr << "typePlayer: " << std::endl << "0 for Magus" << std::endl << "1 for Warrior" << std::endl;
+        std::cerr << "typePlayer: " << std::endl << "0 for Magus" << std::endl << "1 for Warrior" << std::endl << "2 for Rogue" << std::endl;
         return 1;
     }
 
