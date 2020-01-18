5824351442a31f8a5ef8a4ab16da07ce81de489d
VitizLaPeste <lucas.grosjean07@orange.fr>
2020-01-18 18:14:54 +0100

    reworked bad code

--- code/common/Entity.h
+++ code/common/Entity.h
@@ -11,7 +11,7 @@ namespace redsquare
     class Entity
     {
     public:
-        gf::Id m_EntityID;
+        const gf::Id m_EntityID;
         EntitySubType m_TypeOfEntity;
 
         gf::Vector2i m_Pos;
