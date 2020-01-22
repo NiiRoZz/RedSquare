d741ef33e25c5df264c9f108cb487cbd8569c4d6
NiiRoZz <extazziii1000@gmail.com>
2020-01-22 13:30:41 +0100

    Change how data is handled for item

--- code/common/Item.cc
+++ code/common/Item.cc
@@ -7,14 +7,14 @@ namespace redsquare
     , m_Type(type)
     {
         defaultCompatibleSlot();
-        defaultCharacteristic();
+        defaultData();
     }
 
     Item::Item(ItemType type, uint8_t slotMask)
     : m_SlotMask(slotMask)
     , m_Type(type)
     {
-        defaultCharacteristic();
+        defaultData();
     }
 
     ItemType Item::getType() const
@@ -51,7 +51,7 @@ namespace redsquare
         addCompatibleSlot(InventorySlotType::Cargo);
     }
 
-    void Item::defaultCharacteristic()
+    void Item::defaultData()
     {
         switch (m_Type)
         {
@@ -63,14 +63,17 @@ namespace redsquare
 
             default:
             {
-                m_PassiveLifePoint = 0;
-                m_PassiveAttackPoint = 0;
-                m_PassiveDefensePoint = 0;
                 m_GiveLifePoint = 0;
                 m_GiveAttackPoint = 0;
                 m_GiveDefensePoint = 0;
+                m_Useable = false;
                 break;
             }
         }
+    }
+
+    bool Item::isUseable() const
+    {
+        return m_Useable;
     }
 }
