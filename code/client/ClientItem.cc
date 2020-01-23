2e6a49da1599e403a123e19abd6d1960a0c4b1ac
VitizLaPeste <lucas.grosjean07@orange.fr>
2020-01-23 14:33:05 +0100

    Added item sprite

--- code/client/ClientItem.cc
+++ code/client/ClientItem.cc
@@ -22,6 +22,18 @@ namespace redsquare
             case ItemType::Bow1: 
                 return gResourceManager().getTexture("img/Item/Bow/Bow1.png");
                 break;
+            case ItemType::Helmet1: 
+                return gResourceManager().getTexture("img/Item/Helmet/Helmet1.png");
+                break;
+            case ItemType::Shield1: 
+                return gResourceManager().getTexture("img/Item/Shield/Shield1.png");
+                break;
+            case ItemType::Chesplate1: 
+                return gResourceManager().getTexture("img/Item/Chesplate/Chesplate1.png");
+                break;
+            case ItemType::SpellBook1: 
+                return gResourceManager().getTexture("img/Item/SpellBook/SpellBook1.png");
+                break;
         }
 
         return gResourceManager().getTexture("img/attackCursor.png");
