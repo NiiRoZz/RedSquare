423fee1b5f83292d60bde69418b157e1af07100e
Flo <florian.vetter@hotmail.fr>
2020-02-14 15:27:13 +0100

    effefeef

--- code/client/RoomScene.cc
+++ code/client/RoomScene.cc
@@ -5,6 +5,12 @@
 
 #include <gf/Coordinates.h>
 #include <gf/Log.h>
+#include <gf/UI.h>
+#include <gf/Window.h>
+#include <gf/Font.h>
+#include <gf/Color.h>
+#include <gf/Widgets.h>
+#include <gf/Unused.h>
 
 #include <imgui.h>
 #include <imgui_impl_gf.h>
@@ -21,8 +27,21 @@ namespace redsquare
     : gf::Scene(InitialSize)
     , m_Scenes(scenes)
     , m_Network(network)
+    , m_Font(scenes.resources.getFont("font/arial.ttf"))
+    , m_GaucheButton("<===", m_Font, 25)
+    , m_DroiteButton("===>", m_Font, 25)
+    , m_DisplayEntitySubType(EntitySubType::Unknow)
+    , m_InfoText("", m_Font)
     {
         setClearColor(gf::Color::Black);
+
+        m_GaucheButton.setCallback([this]() { displayNextSubEntityTexture(-1); });
+        m_Container.addWidget(m_GaucheButton);
+
+        m_DroiteButton.setCallback([this]() { displayNextSubEntityTexture(1); });
+        m_Container.addWidget(m_DroiteButton);
+
+        displayNextSubEntityTexture(1);
     }
 
     void RoomScene::startRoom()
@@ -33,6 +52,20 @@ namespace redsquare
     void RoomScene::doProcessEvent(gf::Event& event)
     {
         ImGui_ImplGF_ProcessEvent(event);
+
+        switch (event.type) {
+        case gf::EventType::MouseMoved:
+            m_Container.pointTo(m_Scenes.getRenderer().mapPixelToCoords(event.mouseCursor.coords));
+            break;
+
+        case gf::EventType::MouseButtonPressed:
+            m_Container.pointTo(m_Scenes.getRenderer().mapPixelToCoords(event.mouseButton.coords));
+            m_Container.triggerAction();
+            break;
+
+        default:
+            break;
+        }
     }
 
     void RoomScene::doUpdate(gf::Time time)
@@ -114,7 +147,7 @@ namespace redsquare
 
         // UI
         ImGui::NewFrame();
-        ImGui::SetNextWindowPos(ImVec2(position.x, position.y), 0, ImVec2(0.5f, 0.5f));
+        ImGui::SetNextWindowPos(ImVec2(position.x, position.y), 0, ImVec2(0.5f, 1.6f));
         ImGui::SetNextWindowSize(ImVec2(1000.0f, 0.0f));
 
         if (ImGui::Begin("Room", nullptr, DefaultWindowFlags))
@@ -171,11 +204,137 @@ namespace redsquare
         }
         ImGui::End();
 
+        gf::Vector2f MainMenuWindowSizeDescription = coords.getRelativeSize({0.25f,0.25f });
+        gf::Vector2f MainMenuWindowPosDescription = coords.getRelativePoint({ 0.025,0.65f });
+        ImGui::SetNextWindowSize(ImVec2(MainMenuWindowSizeDescription[0], MainMenuWindowSizeDescription[1]));
+        ImGui::SetNextWindowPos(ImVec2(MainMenuWindowPosDescription[0], MainMenuWindowPosDescription[1]));
+        
+        ImGui::Begin("Description Character", nullptr, DefaultWindowFlags);
+        
+        ImVec2 descP=ImGui::GetWindowSize();
+        if(m_DisplayEntitySubType == EntitySubType::Magus)
+        {
+            ImGui::TextWrapped("Magus demolish their foes with incantations. Although they wield powerful offensive spells, mages are fragile and lightly armored, making them particularly vulnerable to close-range attacks. Wise mages make careful use of their spells to keep their foes at a distance or hold them in place.");
+        }
+        if(m_DisplayEntitySubType == EntitySubType::Warrior)
+        {
+            ImGui::TextWrapped("Warriors equip themselves carefully for combat and engage their enemies head-on, letting attacks glance off their heavy armor. They use diverse combat tactics and a wide variety of weapon types to protect their more vulnerable allies.");
+        }
+        if(m_DisplayEntitySubType == EntitySubType::Rogue)
+        {
+            ImGui::TextWrapped("Rogues often initiate combat with a surprise attack from the shadows, leading with vicious melee strikes. When in protracted battles, they utilize a successive combination of carefully chosen attacks to soften the enemy up for a killing blow.");
+        }
+        if(m_DisplayEntitySubType == EntitySubType::Ranger)
+        {
+            ImGui::TextWrapped("Ranger battle their foes at a distance, they nock their arrows between the two eyes. Though their missile weapons are effective at short and long ranges, hunters are also highly mobile.");
+        }
+        if(m_DisplayEntitySubType == EntitySubType::Healer)
+        {
+            ImGui::TextWrapped("Priests use powerful healing magic to fortify themselves and their allies. They also wield powerful offensive spells from a distance, but can be overwhelmed by enemies due to their physical frailty and minimal armor. Experienced priests carefully balance the use of their offensive powers when tasked with keeping their party alive.");
+        }
+        ImGui::SetWindowFontScale(descP[0]/290);
+
+        ImGui::End();
+
+        
+        gf::Vector2f GauchePosButton = coords.getRelativePoint({ 0.35,0.65f });
+        m_GaucheButton.setPosition({ GauchePosButton[0],GauchePosButton[1]});
+        m_GaucheButton.setTextOutlineThickness(1.0f);
+        m_GaucheButton.setAnchor(gf::Anchor::TopLeft);
+        m_GaucheButton.setDefaultTextColor(gf::Color::White);
+        m_GaucheButton.setDefaultTextOutlineColor(gf::Color::Black);
+        m_GaucheButton.setSelectedTextOutlineColor(gf::Color::Black);
+        target.draw(m_GaucheButton,states);
+        
+        gf::Vector2f startPlayerWidgetSize = coords.getRelativeSize({0.20f,0.27f });
+        gf::Vector2f startPlayerWidgetPos = coords.getRelativePoint({ 0.40,0.67f });
+        m_PlayerWidget.setPosition(startPlayerWidgetPos);
+        m_PlayerWidget.setPosition(startPlayerWidgetPos);
+        m_PlayerWidget.setScale(startPlayerWidgetSize/16.f);
+        target.draw(m_PlayerWidget,states);
+
+        float characterSize = coords.getRelativeCharacterSize(0.035f);
+        
+        m_InfoText.setCharacterSize(characterSize);
+        m_InfoText.setColor(gf::Color::White);
+        m_InfoText.setString( "Select a Character");
+        m_InfoText.setPosition(coords.getRelativePoint({0.42f,0.67f}));
+        m_InfoText.setOutlineThickness(characterSize * 0.04f);
+        target.draw(m_InfoText, states);
+
+        gf::Vector2f DroitePosButton = coords.getRelativePoint({ 0.60,0.65f });
+        m_DroiteButton.setPosition({ DroitePosButton[0],DroitePosButton[1]});
+        m_DroiteButton.setTextOutlineThickness(1.0f);
+        m_DroiteButton.setAnchor(gf::Anchor::TopLeft);
+        m_DroiteButton.setDefaultTextColor(gf::Color::White);
+        m_DroiteButton.setDefaultTextOutlineColor(gf::Color::Black);
+        m_DroiteButton.setSelectedTextOutlineColor(gf::Color::Black);
+        target.draw(m_DroiteButton,states);
+
+        gf::Vector2f MainMenuWindowSizeCharac = coords.getRelativeSize({0.25f,0.25f });
+        gf::Vector2f MainMenuWindowPosCharac = coords.getRelativePoint({ 0.73,0.65f });
+        ImGui::SetNextWindowSize(ImVec2(MainMenuWindowSizeCharac[0], MainMenuWindowSizeCharac[1]));
+        ImGui::SetNextWindowPos(ImVec2(MainMenuWindowPosCharac[0], MainMenuWindowPosCharac[1]));
+
+        ImGui::Begin("Characteristic Character", nullptr, DefaultWindowFlags);
+        
+        ImVec2 charP=ImGui::GetWindowSize();
+        if(m_DisplayEntitySubType == EntitySubType::Magus)
+        {
+            std::string text= "Lifepoint : " + std::to_string(MAGUS_LIFEPOINT)+"\n"+"Manapoint : " + std::to_string(MAGUS_MANAPOINT)+"\n"+"Attack : " + std::to_string(MAGUS_ATTACKPOINT)+"\n"+"Defense : " + std::to_string(MAGUS_DEFENSEPOINT)+"\n"+"Range : " + std::to_string(MAGUS_RANGE);
+            ImGui::TextWrapped(text.c_str());
+        }
+        if(m_DisplayEntitySubType == EntitySubType::Warrior)
+        {
+            std::string text= "Lifepoint : " + std::to_string(WARRIOR_LIFEPOINT)+"\n"+"Manapoint : " + std::to_string(WARRIOR_MANAPOINT)+"\n"+"Attack : " + std::to_string(WARRIOR_ATTACKPOINT)+"\n"+"Defense : " + std::to_string(WARRIOR_DEFENSEPOINT)+"\n"+"Range : " + std::to_string(WARRIOR_RANGE);
+            ImGui::TextWrapped(text.c_str());
+        }  
+        if(m_DisplayEntitySubType == EntitySubType::Rogue)
+        {
+            std::string text= "Lifepoint : " + std::to_string(ROGUE_LIFEPOINT)+"\n"+"Manapoint : " + std::to_string(ROGUE_MANAPOINT)+"\n"+"Attack : " + std::to_string(ROGUE_ATTACKPOINT)+"\n"+"Defense : " + std::to_string(ROGUE_DEFENSEPOINT)+"\n"+"Range : " + std::to_string(ROGUE_RANGE);
+            ImGui::TextWrapped(text.c_str());
+        }  
+        if(m_DisplayEntitySubType == EntitySubType::Ranger)
+        {
+            std::string text= "Lifepoint : " + std::to_string(RANGER_LIFEPOINT)+"\n"+"Manapoint : " + std::to_string(RANGER_MANAPOINT)+"\n"+"Attack : " + std::to_string(RANGER_ATTACKPOINT)+"\n"+"Defense : " + std::to_string(RANGER_DEFENSEPOINT)+"\n"+"Range : " + std::to_string(RANGER_RANGE);
+            ImGui::TextWrapped(text.c_str());
+        }  
+        if(m_DisplayEntitySubType == EntitySubType::Healer)
+        {
+            std::string text= "Lifepoint : " + std::to_string(HEALER_LIFEPOINT)+"\n"+"Manapoint : " + std::to_string(HEALER_MANAPOINT)+"\n"+"Attack : " + std::to_string(HEALER_ATTACKPOINT)+"\n"+"Defense : " + std::to_string(HEALER_DEFENSEPOINT)+"\n"+"Range : " + std::to_string(HEALER_RANGE);
+            ImGui::TextWrapped(text.c_str());
+        }          
+        ImGui::SetWindowFontScale(charP[0]/290);
+
+        ImGui::End();
+        
         // Display
         renderWorldEntities(target, states);
         renderHudEntities(target, states);
 
         ImGui::Render();
         ImGui_ImplGF_RenderDrawData(ImGui::GetDrawData());
+    }
+
+    void RoomScene::displayNextSubEntityTexture(int offset)
+    {
+        uint8_t nextIndex = static_cast<uint8_t>(m_DisplayEntitySubType);
+        nextIndex += offset;
+
+        if (nextIndex <= 0u)
+        {
+            nextIndex = 5;
+        }
+
+        if (nextIndex >= 6u)
+        {
+            nextIndex = 1;
+        }
+
+        m_DisplayEntitySubType = static_cast<EntitySubType>(nextIndex);
+        gf::Texture *texture = ClientEntity::loadTexture(m_DisplayEntitySubType);
+        m_PlayerWidget.setDefaultSprite(*texture, gf::RectF::fromPositionSize({ 0.0f, 0.0f }, { 1.0f, 1.0f }));
+        m_PlayerWidget.setDisabledSprite(*texture, gf::RectF::fromPositionSize({ 0.0f, 0.0f }, { 1.0f, 1.0f }));
+        m_PlayerWidget.setSelectedSprite(*texture, gf::RectF::fromPositionSize({ 0.0f, 0.0f }, { 1.0f, 1.0f }));
     }
 }
